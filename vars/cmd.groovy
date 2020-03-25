c5dc2b2723366648bd3137ff6edebd111aa799f5
Nikita Gryzlov <nixel2007@gmail.com>
2020-03-25 17:28:16 +0300

    Рефакторинг

--- vars/cmd.groovy
+++ vars/cmd.groovy
@@ -1,7 +1,7 @@
 def call(String script, boolean returnStatus = false) {
     if (isUnix()) {
-        sh script: "${script}", returnStatus: returnStatus, encoding: "UTF-8"
+        sh script: "$script", returnStatus: returnStatus, encoding: "UTF-8"
     } else {
-        bat script: "chcp 65001 > nul \n${script}", returnStatus: returnStatus, encoding: "UTF-8"
+        bat script: "chcp 65001 > nul \n$script", returnStatus: returnStatus, encoding: "UTF-8"
     }
 }
