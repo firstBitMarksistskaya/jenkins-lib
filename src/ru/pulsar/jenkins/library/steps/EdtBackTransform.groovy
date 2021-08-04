86618f27417094c2c8a593cdc360a422de340d4e
kuzja086 <kozs@tlink.ru>
2021-08-04 08:05:45 +0500

    Исправил пути для конвертаци

--- src/ru/pulsar/jenkins/library/steps/EdtBackTransform.groovy
+++ src/ru/pulsar/jenkins/library/steps/EdtBackTransform.groovy
@@ -8,10 +8,10 @@ import ru.pulsar.jenkins.library.utils.Logger
 
 class EdtBackTransform implements Serializable {
 
-    public static final String PROJECT_NAME = 'pb17' // TODO Брать из srcDir
-    public static final String WORKSPACE = 'build/edt-workspace'
-    public static final String CONFIGURATION_ZIP = 'build/cfgPath.zip'
-    public static final String CONFIGURATION_ZIP_STASH = 'cfgPath-zip'
+    public static final String WORKSPACE = 'build/workcspace'
+    public static final String CONFIGURATION_DIR = 'build/cfg'
+    public static final String CONFIGURATION_ZIP = 'build/cfg.zip'
+    public static final String CONFIGURATION_ZIP_STASH = 'cfg-zip'
 
     private final JobConfiguration config;
 
@@ -31,21 +31,23 @@ class EdtBackTransform implements Serializable {
 
         def env = steps.env();
 
-        def workspaceDir = "$env.WORKSPACE/$WORKSPACE"
-        def configurationRoot = config.srcDir
+        def projectDir = config.srcDir
+        def workspaceDir = "$env.WORKSPACE/$WORKSPACE" 
+        def configurationRoot = "$env.WORKSPACE/$CONFIGURATION_DIR"
 
         steps.createDir(workspaceDir)
+        steps.createDir(configurationRoot)
 
         Logger.println("Конвертация исходников из формата EDT в формат Конфигуратора")
 
-        def ringCommand = "ring edt workspace export --configuration-files '$configurationRoot' --project-name $PROJECT_NAME --workspace-location '$workspaceDir'"
+        def ringCommand = "ring edt workspace export --configuration-files '$configurationRoot' --project $projectDir --workspace-location '$workspaceDir'"
 
         def ringOpts = ['RING_OPTS=-Dfile.encoding=UTF-8 -Dosgi.nl=ru -Duser.language=ru']
         steps.withEnv(ringOpts) {
             steps.cmd(ringCommand)
         }
 
-        steps.zip(WORKSPACE, CONFIGURATION_ZIP)
+        steps.zip(CONFIGURATION_DIR, CONFIGURATION_ZIP)
         steps.stash(CONFIGURATION_ZIP_STASH, CONFIGURATION_ZIP)
     }
 
