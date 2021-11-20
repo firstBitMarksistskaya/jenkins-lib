696f7b1784d995c7ce1b6f7972b272f96a6a4e22
kuzja086 <51016001+kuzja086@users.noreply.github.com>
2021-11-20 21:52:14 +0500

    Update pipeline1C.groovy

--- vars/pipeline1C.groovy
+++ vars/pipeline1C.groovy
@@ -19,7 +19,7 @@ void call() {
 
         options {
             buildDiscarder(logRotator(numToKeepStr: '30'))
-            timeout(time: 2, unit: TimeUnit.HOURS)
+            timeout(time: 5, unit: TimeUnit.HOURS)
             timestamps()
         }
 
