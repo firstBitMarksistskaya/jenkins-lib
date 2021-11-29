5eb48e788ba3d72ed8c750d4cd3adc4a32d2c6bc
v.belyaev <v.belyaev@1ceasy.ru>
2021-11-29 17:15:22 +0300

    fix. ошибки

--- vars/pipeline1C.groovy
+++ vars/pipeline1C.groovy
@@ -66,7 +66,7 @@ void call() {
                             stage('Создание ИБ') {
                                 steps {
                                     configFileProvider([configFile(fileId: 'NETHASP-FILE', targetLocation: '/opt/1cv8/conf/nethasp.ini')]) {
-                                        sh "chown -R usr1cv8:grp1cv8 /opt/1cv8/conf"
+                                        sh "chown -R usr1cv8 /opt/1cv8/conf"
                                         }
                                     createDir('build/out')
 
