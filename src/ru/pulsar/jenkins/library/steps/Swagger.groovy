380027fffa17e8e7faec150c79ed77e5bbb6c227
Sergey Kozinskiy <kozs@tlink.ru>
2021-12-06 08:42:39 +0500

    Test PublishHTML

--- src/ru/pulsar/jenkins/library/steps/Swagger.groovy
+++ src/ru/pulsar/jenkins/library/steps/Swagger.groovy
@@ -1,5 +1,6 @@
 package ru.pulsar.jenkins.library.steps
 
+import groovy.io.FileType
 import ru.pulsar.jenkins.library.IStepExecutor
 import ru.pulsar.jenkins.library.configuration.JobConfiguration
 import ru.pulsar.jenkins.library.ioc.ContextRegistry
@@ -8,6 +9,7 @@ import ru.pulsar.jenkins.library.utils.OscriptModules
 
 class Swagger {
     public static final String OUT = 'build/out/swagger/'
+    public static final String OUT_HTML = 'build/out/swagger/html/'
 
     private final JobConfiguration config;
 
@@ -30,6 +32,22 @@ class Swagger {
         String swaggerPath = OscriptModules.getAppExecutable("swagger");
 
         steps.cmd(swaggerPath + " generate --src-path $config.srcDir --out $OUT")
+
+        def dir = new File(OUT)
+        dir.eachFile(FileType.FILES){
+            String reportdir = "$OUT_HTML$it.name"
+            Logger.println(it.name)
+            Logger.println(reportdir)
+            Logger.println(it.path)
+            steps.cmd("bootprint openapi $it.path $reportdir")
+            publishHTML (target : [allowMissing: false,
+                                   alwaysLinkToLastBuild: true,
+                                   keepAll: true,
+                                   reportDir: $reportdir,
+                                   reportFiles: 'index.html',
+                                   reportName: 'Swagger API',
+                                   reportTitles: 'API $it.name'])
+        }
 
         steps.archiveArtifacts(OUT)
     }
