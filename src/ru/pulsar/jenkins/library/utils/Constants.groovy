584205f3ba5e0f5998a3657c46728549c39d5a4b
v.belyaev <v.belyaev@1ceasy.ru>
2022-12-14 15:35:04 +0300

    fix. косячим

--- src/ru/pulsar/jenkins/library/utils/Constants.groovy
+++ src/ru/pulsar/jenkins/library/utils/Constants.groovy
@@ -3,7 +3,7 @@ import ru.pulsar.jenkins.library.configuration.JobConfiguration
 
 final class Constants {
     Constants(JobConfiguration config) {
-        this.DEFAULT_RING_OPTS = ("RING_OPTS=-Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF8 -Dosgi.nl=ru -Duser.language=ru -Xmx" + config.ringMemory);
-    }
-    public static final String DEFAULT_RING_OPTS; 
+        static this.DEFAULT_RING_OPTS = ("RING_OPTS=-Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF8 -Dosgi.nl=ru -Duser.language=ru -Xmx" + config.ringMemory);
+    }   
+    public static final String DEFAULT_RING_OPTS;    
 }
