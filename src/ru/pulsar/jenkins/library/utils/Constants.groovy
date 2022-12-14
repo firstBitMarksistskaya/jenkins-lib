00f155c32b3f6598677e26391cbc531b7de76a8c
v.belyaev <v.belyaev@1ceasy.ru>
2022-12-14 15:47:15 +0300

    fix. ошибка переопределения константы

--- src/ru/pulsar/jenkins/library/utils/Constants.groovy
+++ src/ru/pulsar/jenkins/library/utils/Constants.groovy
@@ -3,7 +3,7 @@ import ru.pulsar.jenkins.library.configuration.JobConfiguration
 
 final class Constants {
     Constants(JobConfiguration config) {
-        static this.DEFAULT_RING_OPTS = ("RING_OPTS=-Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF8 -Dosgi.nl=ru -Duser.language=ru -Xmx" + config.ringMemory);
+        this.DEFAULT_RING_OPTS = ("RING_OPTS=-Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF8 -Dosgi.nl=ru -Duser.language=ru -Xmx" + config.ringMemory);
     }   
-    public static final String DEFAULT_RING_OPTS;    
+    public static String DEFAULT_RING_OPTS;    
 }
