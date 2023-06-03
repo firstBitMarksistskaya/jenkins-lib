4930715ec746b085fd53c4713d0d36d233932a18
Dima <d.ovcharenko90@gmail.com>
2023-06-03 13:33:04 +0300

    fix bdd

--- src/ru/pulsar/jenkins/library/steps/Bdd.groovy
+++ src/ru/pulsar/jenkins/library/steps/Bdd.groovy
@@ -26,6 +26,7 @@ class Bdd implements Serializable {
             return
         }
 
+        def options = config.bddOptions
         def env = steps.env();
         def srcDir = config.srcDir
         FilePath projectDir = FileUtils.getFilePath("$env.WORKSPACE/$srcDir")
