5e2aedb3c62e61d18951d32a79806beaa6311341
Dima <d.ovcharenko90@gmail.com>
2023-06-12 11:08:01 +0300

    fix class

--- src/ru/pulsar/jenkins/library/steps/Bdd.groovy
+++ src/ru/pulsar/jenkins/library/steps/Bdd.groovy
@@ -39,7 +39,7 @@ class Bdd implements Serializable {
 
             def coverageOpts = config.coverageOptions;
             if (options.coverage) {
-                steps.start("${coverageOpts.dbgsPath} --addr=127.0.0.1 --port=${PortPicker.port()}")
+                steps.start("${coverageOpts.dbgsPath} --addr=127.0.0.1 --port=${PortPicker.getPort()}")
                 steps.start("${coverageOpts.coverage41CPath} start -i DefAlias -u http://127.0.0.1:1550 -P $workspaceDir -s $srcDir -o build/out/bdd-coverage.xml")
             }
 
