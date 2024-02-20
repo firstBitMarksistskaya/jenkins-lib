7714b4bb51f192a899148fe76cd753160715ef2a
ivanovEV <ivanovev@melonfashion.com>
2024-02-20 19:11:15 +0300

    ренейм initMethod -> InitExtentionMethod Добавил значения по умолчанию. Убрал из ридми инфу по синтакс контролю расширений.

--- src/ru/pulsar/jenkins/library/configuration/InitExtentionMethod.groovy
+++ src/ru/pulsar/jenkins/library/configuration/InitExtentionMethod.groovy
@@ -2,7 +2,7 @@ package ru.pulsar.jenkins.library.configuration
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-enum InitMethod {
+enum InitExtentionMethod {
     @JsonProperty("fromSource")
     SOURCE,
 
