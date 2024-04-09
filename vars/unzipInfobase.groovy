e92dc314d3bae354f6fac8e7035e587c69138b25
Dima <d.ovcharenko90@gmail.com>
2024-04-09 15:24:57 +0300

    add safe unstash

--- vars/unzipInfobase.groovy
+++ vars/unzipInfobase.groovy
@@ -3,5 +3,9 @@ import ru.pulsar.jenkins.library.steps.GetExtensions
 def call() {
     unstash '1Cv8.1CD.zip'
     unzip dir: 'build/ib', zipFile: '1Cv8.1CD.zip'
-    unstash GetExtensions.EXTENSIONS_STASH
+    try {
+        unstash GetExtensions.EXTENSIONS_STASH
+    } catch (Exception e) {
+        echo e.toString()
+    }
 }
