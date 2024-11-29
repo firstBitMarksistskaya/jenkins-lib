049951030e0806c07c63dfd07329df7aeebad036
Dima <d.ovcharenko90@gmail.com>
2024-11-29 13:56:20 +0300

    simplify optional unstash

--- vars/unzipInfobase.groovy
+++ vars/unzipInfobase.groovy
@@ -3,9 +3,8 @@ import ru.pulsar.jenkins.library.steps.GetExtensions
 def call() {
     unstash '1Cv8.1CD.zip'
     unzip dir: 'build/ib', zipFile: '1Cv8.1CD.zip'
-    try {
+    catchError {
+        // extensions are optional
         unstash GetExtensions.EXTENSIONS_STASH
-    } catch (Exception e) {
-        echo e.toString()
     }
 }
