77130fb70d43bf29fcafa86c35594e484d65a864
Dima <d.ovcharenko90@gmail.com>
2024-11-30 12:53:10 +0300

    revert catchError

--- vars/unzipInfobase.groovy
+++ vars/unzipInfobase.groovy
@@ -3,8 +3,9 @@ import ru.pulsar.jenkins.library.steps.GetExtensions
 def call() {
     unstash '1Cv8.1CD.zip'
     unzip dir: 'build/ib', zipFile: '1Cv8.1CD.zip'
-    catchError {
-        // extensions are optional
+    try {
         unstash GetExtensions.EXTENSIONS_STASH
+    } catch (Exception e) {
+        echo e.toString()
     }
 }
