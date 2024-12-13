0a78703449ba9c0b87b9e481ebdbc8b2eaf2a9c5
Dima <d.ovcharenko90@gmail.com>
2024-12-13 11:55:03 +0300

    add logging to start step

--- src/ru/pulsar/jenkins/library/steps/Start.groovy
+++ src/ru/pulsar/jenkins/library/steps/Start.groovy
@@ -16,11 +16,31 @@ class Start implements Serializable {
 
     void run() {
         IStepExecutor steps = ContextRegistry.getContext().getStepExecutor()
+        def env = steps.env()
 
+        def executable_name = getExecutableName(executable)
         if (steps.isUnix()) {
-            steps.sh("$executable $params &", false, false , encoding)
+            steps.sh("$executable $params > \"./build/${env.STAGE_NAME}-start-${executable_name}.log 2>&1 &", false, false , encoding)
         } else {
-            steps.bat("chcp 65001 > nul \nstart \"\" /B \"$executable\" $params", false, false, encoding)
+            steps.bat("chcp 65001 > nul \nstart \"\" /B \"$executable\" $params > \"./build/${env.STAGE_NAME}-start-${executable_name}.log\" 2>&1", false, false, encoding)
         }
+    }
+
+    static String getExecutableName(String executable) {
+
+        IStepExecutor steps = ContextRegistry.getContext().getStepExecutor()
+
+        String normalizedPath = executable.replace("\\", "/")
+        String executableName = normalizedPath.substring(normalizedPath.lastIndexOf("/") + 1)
+
+        // Remove the file extension if it exists on Windows systems (e.g., .exe, .cmd, .bat)
+        if (!steps.isUnix()) {
+            int extensionIndex = executableName.lastIndexOf(".")
+            if (extensionIndex != -1) {
+                executableName = executableName.substring(0, extensionIndex)
+            }
+        }
+
+        return executableName
     }
 }
