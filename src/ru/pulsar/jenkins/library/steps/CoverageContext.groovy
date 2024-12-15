4d82101846bec9d6a1153cd0e4a282e636bc846b
Nikita Fedkin <nixel2007@gmail.com>
2024-12-15 15:58:49 +0100

    Update src/ru/pulsar/jenkins/library/steps/CoverageContext.groovy

--- src/ru/pulsar/jenkins/library/steps/CoverageContext.groovy
+++ src/ru/pulsar/jenkins/library/steps/CoverageContext.groovy
@@ -12,7 +12,7 @@ class CoverageContext {
     ArrayList<String> dbgsPids
     ArrayList<String> coverage41CPids
 
-    CoverageContext(String lockableResource, String srcDir, GlobalCoverageOptions coverageOptions, int port, ArrayList<String> dbgsPids, ArrayList<String> coverage41CPids) {
+    CoverageContext(String lockableResource, String srcDir, GlobalCoverageOptions coverageOptions, int port, List<String> dbgsPids, List<String> coverage41CPids) {
         this.lockableResource = lockableResource
         this.srcDir = srcDir
         this.coverageOptions = coverageOptions
