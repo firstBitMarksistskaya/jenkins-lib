34dca070264b3a0753aa820a26630e5afd81587f
Dima <d.ovcharenko90@gmail.com>
2025-01-18 19:18:48 +0300

    fix options

--- src/ru/pulsar/jenkins/library/steps/ZipInfobase.groovy
+++ src/ru/pulsar/jenkins/library/steps/ZipInfobase.groovy
@@ -2,6 +2,7 @@ package ru.pulsar.jenkins.library.steps
 
 import hudson.model.Result
 import ru.pulsar.jenkins.library.IStepExecutor
+import ru.pulsar.jenkins.library.configuration.ArchiveInfobaseOptions
 import ru.pulsar.jenkins.library.configuration.JobConfiguration
 import ru.pulsar.jenkins.library.ioc.ContextRegistry
 import ru.pulsar.jenkins.library.utils.Logger
@@ -24,7 +25,7 @@ class ZipInfobase implements Serializable {
         def currentBuild = steps.currentBuild()
         def currentResult = Result.fromString(currentBuild.getCurrentResult())
 
-        def archiveInfobaseOptions = options.archiveInfobase
+        def archiveInfobaseOptions = getArchiveInfobaseOptionsForStage(config, stage)
 
         def archiveInfobase = false
         if (archiveInfobaseOptions.onAlways
@@ -37,5 +38,20 @@ class ZipInfobase implements Serializable {
         def archiveName = "1Cv8.1CD.${stage}.zip"
         steps.zip('build/ib', '1Cv8.1CD', archiveName, archiveInfobase)
         steps.stash(archiveName, archiveName, false)
+    }
+
+    private static ArchiveInfobaseOptions getArchiveInfobaseOptionsForStage(JobConfiguration config, String stageName) {
+
+        def defaultOptions = new ArchiveInfobaseOptions()
+        if (!stageName) {
+            return defaultOptions
+        }
+
+        try {
+            return config."${stageName}Options".archiveInfobase
+        } catch(Exception e) {
+            Logger.println(e.message)
+           return defaultOptions
+        }
     }
 }
